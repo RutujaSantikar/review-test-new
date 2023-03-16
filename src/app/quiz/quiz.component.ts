f3e3a25d9246512990761b834ac56bc602e12942
SwatiVersatalia <swati@versatalialabs.com>
2023-03-16 11:51:49 +0530

    disable button

--- src/app/quiz/quiz.component.ts
+++ src/app/quiz/quiz.component.ts
@@ -18,10 +18,15 @@ export class QuizComponent implements OnInit {
   nquestion: any;
   nquestion1: any;
   correctOption: any;
+  selectedValue: any;
+  selectedValueImage: any;
+
+
 
   constructor(private qzService: QzService) {}
 
   ngOnInit() {
+
     this.qzService.getQuestions().subscribe((response: any) => {
       this.getData1 = response;
       console.log(this.getData1);
@@ -34,21 +39,27 @@ export class QuizComponent implements OnInit {
     this.showUplod = true;
     this.correctOption = 'Yes';
     console.log(event);
+    this.selectedValue = event;
+    console.log(this.selectedValue);
+
   }
   noRadioChange(event: any) {
     this.correctOption = 'No';
     console.log(event);
+    this.selectedValue = event;
+    console.log(this.selectedValue)
   }
 
   nextQuestion() {
+
     for (let i = 0; i < this.getData1.length; i++) {
       if (this.getData.qsOption1.en == this.correctOption && this.imageUrl) {
         this.imageUrl = '';
-        // this.showImage = false;
         this.showUplod = false;
         this.nquestion = this.getData.qsOption1NextQuestion;
         console.log(this.nquestion);
         this.getData = this.getData1[this.nquestion];
+        this.selectedValue = '';
         console.log(this.getData);
       } else if (this.getData.qsOption2.en == this.correctOption) {
         this.nquestion = this.getData.qsOption2NextQuestion;
@@ -60,6 +71,7 @@ export class QuizComponent implements OnInit {
   }
 
   uplodFile(event: any) {
+
     this.showImage = true;
     const files = event.target.files;
     const file = files[0];
